c0f281d3db424ab909bd5ad2b4abb2fc157a1161
EduardArteni <100022914+ArteniTeo@users.noreply.github.com>
2023-01-03 17:31:32 +0200

    Some magical stuff
    Changed it so the parameters are inside the body and not in the URL. Some witchcraft right here.

--- sign-up.js
+++ sign-up.js
@@ -8,13 +8,29 @@ const error_display_sign_up = document.getElementById('error_display_sign_up');
 sign_up_btn.addEventListener('click', signIn);
 
 function signIn() {
-    if (username_sign_up.value == "" && password_sign_up.value == "") {
-        error_display_sign_up.innerHTML = "please enter username and password";
+    if (username_sign_up.value == "" || password_sign_up.value == "" || first_name_sign_up.value == "" || last_name_sign_up.value == "") {
+        error_display_sign_up.innerHTML = "please enter username , password , first name and last name";
     } else {
         error_display_sign_up.innerHTML = "";
-        fetch(`http://127.0.0.1:8080/signup?username=${username_sign_up.value}&password=${password_sign_up.value}&first_name=${first_name_sign_up.value}&last_name=${last_name_sign_up.value}`, {
-            method: 'POST',
-        }).then(res => res.json()).then(data => {
+
+        const body = {
+            "username": username_sign_up.value,
+            "password": password_sign_up.value,
+            "firstName": first_name_sign_up.value,
+            "lastName": last_name_sign_up.value
+        };
+        
+          const stringBody = JSON.stringify(body);
+        
+          console.log(stringBody);
+        
+          fetch('http://127.0.0.1:8080/signup', 
+          {method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: stringBody
+          }).then(res => res.json()).then(data => {
             if (data.id > 0) {
                 
                 localStorage.setItem("loggedInUserUsername", data.username);
