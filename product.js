1bd97cf255086bca38d92f09b4597616541cefe4
EduardArteni <100022914+ArteniTeo@users.noreply.github.com>
2023-05-02 21:59:30 +0300

    Added remove button from shopping cart

--- product.js
+++ product.js
@@ -1,15 +1,14 @@
-const product_name_tag = document.querySelector('#product-name-tag');
 const urlParams = new URLSearchParams(window.location.search);
 let prodId = urlParams.get('id');
 
-
-
+const  product_name = document.getElementById("product_name");
+const  product_price = document.getElementById("product_price");
 
 fetch(`http://127.0.0.1:8080/product?id=${prodId}`, {
   method: 'GET',
 }).then(res => res.json()).then(data => {
   console.log(data);
+  product_name.innerHTML = `<b>${data.productName}</b>`;
+  product_price.innerHTML = data.price;
 });
 
-
-product_name_tag.innerHTML = "Current product id -> " + prodId + " :";
--- shopping-cart.js
+++ shopping-cart.js
@@ -1,7 +1,9 @@
 fetch(`http://127.0.0.1:8080/shopping_cart_item?id=${localStorage.getItem('loggedInUserID')}`, {
   method: 'GET',
 }).then(res => res.json()).then(data => {
+  console.log(data);
   data.forEach(element => {
+
     console.log(element);
 
     var div = document.createElement("div");
@@ -10,7 +12,6 @@ fetch(`http://127.0.0.1:8080/shopping_cart_item?id=${localStorage.getItem('logge
 
     const a = document.createElement("a");
     a.innerHTML =  `${element.productName}  `
-    //a.href = `product.html`
     a.href = `product.html?id=${element.productId}`
 
     const a1 = document.createElement("a");
@@ -19,14 +20,27 @@ fetch(`http://127.0.0.1:8080/shopping_cart_item?id=${localStorage.getItem('logge
     const a2 = document.createElement("a");
     a2.innerHTML = `qty: ${element.quantity}`;
 
-    div.style.width = "210px";
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = "remove"; 
+
+    div.style.width = "280px";
     div.style.height = "25px"; 
     div.style.background = "red";
     div.style.color = "yellow";
+    removeButton.id=element.id;
 
     div.appendChild(a);
     div.appendChild(a1);
     div.appendChild(a2);
+    div.appendChild(removeButton);
+
+    removeButton.addEventListener("click", function() { 
+      fetch(`http://127.0.0.1:8080/shopping_cart_item?id=${removeButton.id}`, {
+        method: 'DELETE',
+      });
+      location. reload();
+    });
+    
     document.body.appendChild(div);
   });
 });
